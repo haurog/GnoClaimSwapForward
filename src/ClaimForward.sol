6410ddecae8343750b62f045e9e25c534023c4a3
haurog <36535774+haurog@users.noreply.github.com>
2023-11-08 16:18:19 +0100

    Change interface names

--- src/ClaimForward.sol
+++ src/ClaimForward.sol
@@ -5,16 +5,30 @@ import "./interfaces/Interfaces.sol";
 import "forge-std/console.sol";
 
 contract ClaimForward {
-
     address private GBCDepositContractAddress =
         0x0B98057eA310F4d31F2a452B414647007d1645d9;
+    address private GNOTokenAddress =
+         0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb;
+
+    GBCDepositContract depositContract =
+        GBCDepositContract(GBCDepositContractAddress);
+    GBCDepositContractVariables depositContractVariables =
+        GBCDepositContractVariables(GBCDepositContractAddress);
 
     function getWithdrawableAmount(
         address claimAddress
     ) public view returns (uint256) {
-        GBCDepositContract depositContract = GBCDepositContract(
-            GBCDepositContractAddress
-        );
-        return depositContract.withdrawableAmount(claimAddress);
+        return depositContractVariables.withdrawableAmount(claimAddress);
     }
+
+    function claimWithdrawal(address claimAddress) public {
+        depositContract.claimWithdrawal(claimAddress);
+    }
+
+    // function claimAndForward(address claimAddress) public {
+    //     uint256 amount = getWithdrawableAmount(claimAddress);
+    //     claimWithdrawal(claimAddress);
+    //     IERC20(GNOAddress).transfer(destinationAddress, amount);
+
+    // }
 }
