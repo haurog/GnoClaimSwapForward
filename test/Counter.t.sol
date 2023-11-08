738972e21ab3c8c12640a0eaaab3929c2fd1b7f1
haurog <36535774+haurog@users.noreply.github.com>
2023-11-08 13:32:43 +0100

    Add currently correct gwei number into assert

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -37,7 +37,7 @@ contract CounterTest is Test {
     function test_withdrawableAmount() public {
         address claimAddress = 0x1c0AcCc24e1549125b5b3c14D999D3a496Afbdb1;
         uint256 withdrawableAmount = counter.getWithdrawableAmount(claimAddress);
-        assertEq(withdrawableAmount, 10);
+        assertEq(withdrawableAmount, 54782517406250000);
     }
 
 }
