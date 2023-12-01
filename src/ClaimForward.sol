6a1f07a33199dcb0bc54fc84346fd389cf2736dc
haurog <36535774+haurog@users.noreply.github.com>
2023-12-01 10:26:29 +0100

    preparing batch swap steps.

--- src/ClaimForward.sol
+++ src/ClaimForward.sol
@@ -8,6 +8,16 @@ import "forge-std/console.sol";
 contract ClaimForward {
     using SafeERC20 for IERC20;
 
+    struct BatchSwapStep {
+        bytes32 poolId;
+        uint256 assetInIndex;
+        uint256 assetOutIndex;
+        uint256 amount;
+        bytes userData;
+        }
+
+    BatchSwapStep[] batchSwapSteps;
+
     address private GBCDepositContractAddress =
         0x0B98057eA310F4d31F2a452B414647007d1645d9;
     address private GNOTokenAddress =
@@ -19,6 +29,8 @@ contract ClaimForward {
         GBCDepositContract(GBCDepositContractAddress);
     GBCDepositContractVariables depositContractVariables =
         GBCDepositContractVariables(GBCDepositContractAddress);
+
+
 
     function getWithdrawableAmount(
         address claimAddress
@@ -35,4 +47,40 @@ contract ClaimForward {
         claimWithdrawal(claimAddress);
         IERC20(GNOTokenAddress).transferFrom(claimAddress, destinationAddress, withdrawableAmount);
     }
+
+
+    function swap(uint256 withdrawableAmount) public {
+
+
+
+        BatchSwapStep memory step1 = BatchSwapStep({
+                poolId: 0x4683e340a8049261057d5ab1b29c8d840e75695e00020000000000000000005a,
+                assetInIndex: 0,
+                assetOutIndex: 1,
+                amount: withdrawableAmount,
+                userData: ""
+                });
+        BatchSwapStep memory step2 = BatchSwapStep({
+                poolId: 0xbc2acf5e821c5c9f8667a36bb1131dad26ed64f9000200000000000000000063,
+                assetInIndex: 1,
+                assetOutIndex: 2,
+                amount: 0,
+                userData: ""
+                });
+        BatchSwapStep memory step3 = BatchSwapStep({
+                poolId: 0xdd439304a77f54b1f7854751ac1169b279591ef7000000000000000000000064,
+                assetInIndex: 2,
+                assetOutIndex: 3,
+                amount: 0,
+                userData: ""
+                });
+        batchSwapSteps.push(step1);
+        batchSwapSteps.push(step2);
+        batchSwapSteps.push(step3);
+
+
+
+    }
+
+
 }
